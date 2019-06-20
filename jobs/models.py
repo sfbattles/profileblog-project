46ee68713506e17825101fc32eefe25dc0ff7feb
Richard Long <richard.long.e@gmail.com>
2019-06-20 10:01:20 -0400

    updated Job models

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
+    class Meta:
+        db_table = 'Job'
+
+    def __str__(self):
+        return str(self.name)
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,10 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'portfolio',
+        'USER': 'root',
+        'PASSWORD': '12345678'
     }
 }
 
@@ -118,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media'),
+MEDIA_URL = '/media/'
