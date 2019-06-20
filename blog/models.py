aa8aeaa71b51a072a5ae146e758ec82f2c312087
Richard Long <richard.long.e@gmail.com>
2019-06-20 13:06:15 -0400

    updated the jobs application

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(auto_now=True)
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/blog/')
+    class Meta:
+        db_table = 'Blog'
+
+    def __str__(self):
+        return str(self.title)
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+import jobs.views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),    
+    path('', jobs.views.home, name='home'),        
 ]
 
 if settings.DEBUG:
